c1973dc7e71b6358ebf7585f9b7a8a984a15ade3
kevamacal <kevamacal@alum.us.es>
2026-02-26 17:02:05 +0100

    fix: arreglado mensaje de aviso por versión antigua

--- ingestar_documentos.py
+++ ingestar_documentos.py
@@ -4,7 +4,7 @@ import sys
 from langchain_community.document_loaders import PyMuPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings 
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
